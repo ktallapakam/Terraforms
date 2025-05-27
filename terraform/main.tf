8c627fffa0f7aa17c5b40589977104059f67db34
Kishore <ktallapakam@gmail.com>
2025-05-27 09:38:17 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"  # Change as needed
+  region = "us-east-1"
 }
 
 resource "aws_security_group" "secGroup" {
