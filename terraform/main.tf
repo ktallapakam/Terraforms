b258a48272fb0101fa03efe96f8ae3232ccdf255
Kishore <ktallapakam@gmail.com>
2025-05-27 09:32:11 +0530

    main.tf added

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-east-1"  # Change as needed
+}
+
+resource "aws_security_group" "secGroup" {
+  name        = "secGroup"
+  description = "Allow SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "Ec2Instance" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  security_groups = [aws_security_group.secGroup.name]
+
+  tags = {
+    Name = "Ec2Instance"
+  }
+}
